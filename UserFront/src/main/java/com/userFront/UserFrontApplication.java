a712cbff382bb0a33806f868d12402a33cc35972
tienhngnguyen <tienhngnguyen@gmail.com>
2017-03-21 17:09:34 +0100

    Update UserFrontApplication.java

--- UserFront/src/main/java/com/userFront/UserFrontApplication.java
+++ UserFront/src/main/java/com/userFront/UserFrontApplication.java
@@ -2,7 +2,7 @@ package com.userFront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//PUSH!! ++++
+
 @SpringBootApplication
 public class UserFrontApplication {
 
